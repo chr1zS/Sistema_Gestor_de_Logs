d6b126e58c14e617a5909c1a02371e42dfc58361
chr1zS <77061855+chr1zS@users.noreply.github.com>
2023-06-04 01:08:37 +0200

    Update elk_install.sh

--- elk_install.sh
+++ elk_install.sh
@@ -1,66 +1,77 @@
 #!/bin/bash
 #############################################################
-# Programa: elk_install.sh									#
+# Programa: elk_install.sh									                #
 # Objetivo: Instalación de Elasticsearch, Logstash y Kibana	#
-# Autor: Christian Suárez									#
-# Fecha: 29/05/2023											#
+# Autor: Christian Suárez									                  #
+# Fecha: 29/05/2023											                    #
 #############################################################
 
-#Recomendación: ejecutar el script como root
+#Ejecutar el script como root
 
-#PRIMERA FASE: preparación de paquetes y conexiones
-#--------------------------------------------------
+echo PRIMERA FASE: preparación de paquetes y conexiones
+echo --------------------------------------------------
 
-#Importción de clave publica PGP de elasticsearch
+echo Importción de clave publica PGP de elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
 
-#Instalación de paquete transport
+echoInstalación de paquete transport
 apt-get -y install apt-transport-https
-#Se guarda el repositorio de elasticsearch
+echo Se guarda el repositorio de elasticsearch
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
-#Actualización de paquetes y repositorio
+echo Actualización de paquetes y repositorio
 apt-get -y update && apt-get -y dist-upgrade
 
 
+echo SEGUNDA FASE: instalación de aplicativos
+echo ----------------------------------------
 
-#SEGUNDA FASE: instalación de aplicativos
-#----------------------------------------
-
-#Instalación de Elasticsearch
+echo Instalación de Elasticsearch
+echo ----------------------------
 apt-get -y install elasticsearch
-#Instalación de Kibana
+
+echo Instalación de Kibana
+echo ---------------------
 apt-get -y install kibana
-#Instalación de Logstash
+
+echo Instalación de Logstash
+echo -----------------------
 apt-get -y install logstash
 
-#Poner los tres aplicativos como servicios
+echo Configuración de aplicativos como servicios
+echo -------------------------------------------
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
 /bin/systemctl enable kibana.service
 /bin/systemctl enable logstash.service
 
 
-#TERCERA FASE: Configuración y aranque de servicios
-#--------------------------------------------------
+echo TERCERA FASE: Arranque de servicios
+echo echo ------------------------------
 
-#Iniciamos Elasticsearch
+echo Iniciamos Elasticsearch
 systemctl start elasticsearch.service
 
-#Paramos Kibana (en caso de encontrarse iniciado)
+echo Paramos Kibana (en caso de encontrarse iniciado)
 systemctl stop kibana.service
-#Generamos el enrollment token para conectar Elasticsearch con Kibana
+
+echo Generamos el enrollment token para conectar Elasticsearch con Kibana
 /usr/share/elasticsearch/bin/elasticsearch-create-enrollment-token -s kibana --url "http://localhost:5601" > /tmp/enrollment_token
-#Iniciamos Kibana 
+echo El enrollment token se ha guardado temrporalmente en /tmp/enrollment_token. Si se reinicia el servidor, el enrollmento token se perderá
+
+echo Iniciamos Kibana 
 systemctl start kibana.service
 
-#Iniciamos Logstash
+echo Iniciamos Logstash
 systemctl start logstash.service
 
-#Cambiamos password y la almacenamos en un fichero temporal
-# Esta clave se necesitará para comprobaciones
-/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic > /tmp/elasticpasswd
-#Comprobamos elasticsearch con un curl
-curl --cacert /etc/elasticsearch/certs/http_ca.crt -u elastic:'cat /tmp/elasticpasswd' https://localhost:9200
+echo Cambiamos password y la almacenamos en un fichero temporal
+# Esta clave se necesitará para comprobaciones y para acceder a Elastic
+/usr/share/elasticsearch/bin/elasticsearch-reset-password -u elastic -s > /tmp/elasticpasswd
+echo La password se ha guardado temrporalmente en /tmp/elasticpasswd. Si se reinicia el servidor, el fichero elasticpasswd se perderá
+
+echo Comprobamos elasticsearch con un curl
+PASS=$(cat /tmp/elasticpasswd)
+curl --cacert /etc/elasticsearch/certs/http_ca.crt -u elastic:$PASS https://localhost:9200
 
 #Finalizamos el script
 exit 0
